7e885078e1c623b6be8954b281d8ba1a6eda6a2c
luquiiya <lucas.rey.pach@gmail.com>
2025-12-09 17:01:16 +0000

    rama actualizada

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ from modules.backend import tickers_by_issuer
 
 if __name__ == "__main__":
     
-    # obtener tickers de ishares
+    # tickers de ishares
     tickers = tickers_by_issuer(issuer="iShares")
    
    
-    #portfolio de renta fija
+    #portfolio RENTA FIJA
     tickers_rf = tickers[tickers["CATEGORIA"]=="ETF RF"]
     list_tickers_rf = list(tickers_rf["TICKER"])
-    #portfolio de renta variable
+    #portfolio RENTA VARIABLE
     tickers_rv = tickers[tickers["CATEGORIA"]=="ETF RV"]
     list_tickers_rv = list(tickers_rv["TICKER"])
 
-    #Rango de fechas
+    #Fechas
     start= "2024-01-01"
     end= "2024-12-31"
 
@@ -25,16 +25,16 @@ if __name__ == "__main__":
     lst= []
     for portafolio in [list_tickers_rf,list_tickers_rv]:
         print(portafolio)
-        #obtener retornos
+        #retornos
         df = portfolio_returns(tickers=portafolio,start=start, end=end)
        
 
         vector_w = np.array([1/len(portafolio)]* len(portafolio))
       
-        #calcular volatilidad
+        # volatilidad
         sigma = portfolio_volatility(df=df, vector_w= vector_w)
        
-        # calcular Var
+        #  Var
         var = VaR(sigma=sigma, confidence=confidence)
         var = np.abs(var)
         var_mensual = var * np.sqrt(20)
